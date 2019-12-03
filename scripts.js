a4cf6fcb9563f060f94f89195432e10210db67c1
LeeEnck <eaglesfootball33@gmail.com>
2019-12-03 16:20:00 -0500

    making the frog.png appear on screen

--- scripts.js
+++ scripts.js
@@ -1,2 +1,21 @@
-let canvas = getElementById('canvas');
-let ctx = canvas.getContext('2d');
+let canvas = document.getElementsById('canvas');
+let ctx = canvas.context('2d');
+
+let frog = new image(); frog.src = "frog.png";
+let sx = 0;
+let sy = 0;
+let swidth = 50;
+let sheight = 50;
+let x = 30;
+let y = 450;
+let width = 25;
+let height = 25;
+
+
+function drawFrog(){
+  ctx.drawimage(frog, sx, sy, swidth, sheight, x, y, width, height);
+}
+
+function draw(){
+  drawFrog();
+}
