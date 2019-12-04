1dbce47ade839efca08c9e099a2fae6171a27bec
LeeEnck <eaglesfootball33@gmail.com>
2019-12-04 14:59:35 -0500

    scrapped canvas idea

--- scripts.js
+++ scripts.js
@@ -1,4 +1,45 @@
-const canvas = document.getElementById('canvas');
+const frogger = document.getElementById('frogger')
+let froggerY = 650
+frogger.style.top = froggerY + "px";
+let froggerX = 0;
+frogger.style.left = froggerX + "px";
+
+function updateFroggerPosition() {
+  frogger.style.left = froggerX + "px";
+  frogger.style.top = froggerY + "px";
+}
+
+
+window.addEventListener("keydown", event => {
+  let button = event.key;
+  switch (button) {
+    case "w":
+      froggerY = froggerY - 50;
+      updateFroggerPosition()
+      break;
+    case "a":
+      froggerX = froggerX - 75;
+      updateFroggerPosition()
+      break;
+    case "s":
+      froggerY = froggerY + 50;
+      updateFroggerPosition()
+      break;
+    case "d":
+      froggerX = froggerX + 75;
+      updateFroggerPosition()
+      break;
+  }
+
+});
+
+
+
+
+
+
+// IDEA: BOTTOM IS OUTDATED CANVAS IDEAS
+/* const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 function drawBackground() {
   //grass
@@ -41,16 +82,35 @@ let sx = 0;
 let sy = 0;
 let swidth = 50;
 let sheight = (700 / 11);
-let x = 0;
+let x = 10;
 let y = 350;
 let width = 50;
 let height = (700 / 11);
 function drawFrog() {
   ctx.drawImage(frog, sx, sy, swidth, sheight, x, y, width, height);
 }
+//player movement
+window.addEventListener("keydown", event => {
+  let button = event.key;
+  switch (button) {
+    case "w":
+      y = y - 50;
+      break;
+    case "a":
+      x = x - (700 / 11);
+      break;
+    case "s":
+      y = y + 50;
+      break;
+    case "d":
+      x = x + (700 / 11);
+      break;
+  }
+});
 function draw() {
   drawBackground();
   drawFrog();
   requestAnimationFrame(draw);
 }
 draw();
+*/
